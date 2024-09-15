a21f1ad0559c22b69ac6769d2b03a25b91551a97
ankit2061 <codingjourney18@gmail.com>
2024-09-15 18:00:07 +0530

    SignUp Page in feature 2

--- app/src/main/java/com/example/studentshub/SignUpPage.java
+++ app/src/main/java/com/example/studentshub/SignUpPage.java
@@ -1,10 +1,12 @@
 package com.example.studentshub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,12 +19,15 @@ public class SignUpPage extends AppCompatActivity {
     String item[]={"Student","Teacher","Mentor"};
     AutoCompleteTextView autoCompleteTextView;
     ArrayAdapter<String> adapterItems;
+    Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up_page);
+
+        signin=findViewById(R.id.signinbutton);
 
         autoCompleteTextView =findViewById(R.id.autoComplete);
         adapterItems=new ArrayAdapter<String>(this, R.layout.list_item,item );
@@ -33,6 +38,13 @@ public class SignUpPage extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String item=adapterView.getItemAtPosition(i).toString();
                 Toast.makeText(SignUpPage.this,"Category: "+item,Toast.LENGTH_SHORT).show();
+            }
+        });
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(SignUpPage.this, MainActivity2.class);
+                startActivity(intent2);
             }
         });
     }
